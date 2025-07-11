0cd66d29f4895b04fc9f43f3d9c2790658048620
ArielPaltanC <146782047+ArielPaltanC@users.noreply.github.com>
2025-07-10 19:24:38 -0500

    un comit

---
+++ src/Controllers/Graph.java
@@ -0,0 +1,45 @@
+package Controllers;
+
+import java.util.Set;
+
+import org.w3c.dom.Node;
+
+public class Graph {
+    private Set<Node> nodes;
+    
+    public Graph(){
+
+    }
+
+    public Node addNode(int value){
+
+    }
+
+    public void addEdge(Node src, Node dest){
+
+    }
+
+    public void printlGraph(){
+
+    }
+
+    public void getDFS(Node startNode){
+
+    }
+
+    private void getDFS(Node node, boolean[] visited){
+
+    }
+
+    public void getBFS(Node startNode){
+
+    }
+
+    public int[][] getAdjacencyMatrix(){
+
+    }
+
+    public void printlAdjacencyMatrix(){
+
+    }
+}
